900fd77ab645931875c88d64dde378e4c2fc1353
halilkocaoz <halil.i.kocaoz@gmail.com>
2022-03-10 18:04:24 +0300

    add food model

---
+++ Cu.Yemekhane.API/Cu.Yemekhane.API.Web/Models/Food.cs
@@ -0,0 +1,13 @@
+namespace Cu.Yemekhane.API.Web.Models;
+
+public class Food
+{
+    public Food(string name, int calories)
+    {
+        Name = name;
+        Calories = calories;
+    }
+
+    public string Name { get; private set; }
+    public int Calories { get; private set; }
+}
