6f0328f8af99adef0a60a9ebf18c9bd4810fb80a
halilkocaoz <halil.i.kocaoz@gmail.com>
2022-03-11 23:37:27 +0300

    update menucontroller to & apiresponse

--- Cu.Yemekhane.API/Controllers/MenuController.cs
+++ Cu.Yemekhane.API/Controllers/MenuController.cs
@@ -3,29 +3,21 @@ using Cu.Yemekhane.Common;
 using Cu.Yemekhane.Common.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Cu.Yemekhane.API.Controllers
+namespace Cu.Yemekhane.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MenuController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class MenuController : ControllerBase
-    {
-        private readonly IMenuService _menuService;
+    private readonly IMenuService _menuService;
+    public MenuController(IMenuService menuService)
+        => _menuService = menuService;
 
-        public MenuController(IMenuService menuService)
-        {
-            _menuService = menuService;
-        }
+    [HttpGet]
+    public ApiResponse<List<Menu>> GetMenus()
+        => _menuService.GetMenus();
 
-        [HttpGet]
-        public ApiResponse<List<Menu>> GetMenus()
-        {
-            return _menuService.GetMenus();
-        }
-
-        [HttpGet("{date}")]
-        public ApiResponse<Menu> GetMenu([FromRoute] string date)
-        {
-            return _menuService.GetMenuByDate(date);
-        }
-    }
+    [HttpGet("{date}")]
+    public ApiResponse<Menu> GetMenu([FromRoute] string date)
+        => _menuService.GetMenuByDate(date);
 }
--- Cu.Yemekhane.Common/ApiResponse.cs
+++ Cu.Yemekhane.Common/ApiResponse.cs
@@ -2,6 +2,7 @@
 namespace Cu.Yemekhane.Common;
 public struct ApiResponse<T>
 {
+    public bool Success => string.IsNullOrEmpty(ErrorMessage) ? true : false;
     public string ErrorMessage { get; set; }
     public T? Data { get; set; }
 }
