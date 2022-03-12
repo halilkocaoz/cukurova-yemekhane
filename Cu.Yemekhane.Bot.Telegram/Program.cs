3e670bfc9f2d5c9de2312ab575d69a7b1e991c09
halilkocaoz <halil.i.kocaoz@gmail.com>
2022-03-12 15:42:42 +0300

    init telegram.bot project as console app

--- Cu.Yemekhane.Bot.Telegram/Program.cs
+++ Cu.Yemekhane.Bot.Telegram/Program.cs
@@ -1,25 +1,95 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Microsoft.Extensions.DependencyInjection;
+using Cu.Yemekhane.Common.Services;
 
-// Add services to the container.
+var serviceProvider = new ServiceCollection()
+    .AddSingleton<IWebApiService, WebApiService>()
+    .BuildServiceProvider();
+var webApiService = serviceProvider.GetService<IWebApiService>();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var botClient = new TelegramBotClient("5241998179:AAFPX3Cv4DoKQhxScTLCtKbt9BbyD4DFIxo");
 
-var app = builder.Build();
+using var cts = new CancellationTokenSource();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var receiverOptions = new ReceiverOptions { AllowedUpdates = { } };
+
+botClient.StartReceiving(HandleUpdateAsync, HandleErrorAsync, receiverOptions, cts.Token);
+
+Console.ReadLine();
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    if (update.Type != UpdateType.Message)
+        return;
+    if (update.Message!.Type != MessageType.Text)
+        return;
+
+    var chatId = update.Message.Chat.Id;
+    var messageText = update.Message.Text;
+
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+    var messageTextArrs = messageText.Split(' ');
+    string todayAsString = System.DateTime.Now.ToString("dd.MM.yyyy");
+    string selectedDay = string.Empty;
+    string helpCommand = "Sana Çukurova Üniversitesinin yemekhane menülerine ulaşman konusunda yardımcı olabilirim.\n" +
+        "/today komutu ile bugünün menüsüne ulaşabilirsin\n" +
+        $"/menu {System.DateTime.Now.ToString("dd.MM.yyyy")} ile herhangi bir günün menüsüne ulaşabilirsin\n" +
+        "/source komutu ile projenin kaynağına ulaşabilirsin\n";
+    switch (messageTextArrs[0])
+    {
+        case "/start":
+            await botClient.SendTextMessageAsync(chatId,
+                "Heyyo! Ben Cu.Yemekhane.Bot.Telegram,\n" +
+                helpCommand
+                + "\n https://github.com/halilkocaoz/cu-yemekhane");
+            break;
+        case "/today":
+            string menu = await getMenuMessage(todayAsString);
+            await botClient.SendTextMessageAsync(chatId, menu);
+            break;
+        case "/menu":
+            selectedDay = messageTextArrs.Length > 1 ? messageTextArrs[1] : todayAsString;
+            menu = await getMenuMessage(selectedDay);
+            await botClient.SendTextMessageAsync(chatId, menu);
+            break;
+        case "/source":
+            await botClient.SendTextMessageAsync(chatId, "https://github.com/halilkocaoz/cu-yemekhane");
+            break;
+        case "/help":
+            await botClient.SendTextMessageAsync(chatId, helpCommand);
+            break;
+        default:
+            await botClient.SendTextMessageAsync(chatId, "Seni anlamadım. Belki ben biraz aptalımdır veya sen, kim bilir.\n/help");
+            break;
+    };
 }
 
-app.UseHttpsRedirection();
+async Task<string> getMenuMessage(string date)
+{
+    var response = await webApiService.GetMenu(date);
+    string detail = string.Empty;
+    if (string.IsNullOrEmpty(response.ErrorMessage))
+        detail = response.Data is null ? "Bugün için menü bulunamadı" : response.Data.Detail;
+    else
+        detail = response.ErrorMessage;
+    return detail;
+}
 
-app.UseAuthorization();
+Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
 
-app.MapControllers();
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
 
-app.Run();
