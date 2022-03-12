ad42ed677724e9672e91eafcc6de6c7bd6d12758
halilkocaoz <halil.i.kocaoz@gmail.com>
2022-03-12 14:26:35 +0300

    move webapiservice to common

---
+++ Cu.Yemekhane.Common/Services/WebApiService.cs
@@ -0,0 +1,22 @@
+using Cu.Yemekhane.Common.Data.Models;
+using Flurl;
+using Flurl.Http;
+
+namespace Cu.Yemekhane.Common.Services;
+
+public interface IWebApiService
+{
+    Task<ApiResponse<List<Menu>>> GetMenus();
+    Task<ApiResponse<Menu>> GetMenu(string date);
+}
+
+public class WebApiService : IWebApiService
+{
+    private const string BaseUrl = "https://cu-yemekhane.herokuapp.com/";
+
+    public Task<ApiResponse<Menu>> GetMenu(string date)
+        => BaseUrl.AppendPathSegment("menu").AppendPathSegment(date).GetJsonAsync<ApiResponse<Menu>>();
+
+    public async Task<ApiResponse<List<Menu>>> GetMenus()
+        => await BaseUrl.AppendPathSegment("menu").GetJsonAsync<ApiResponse<List<Menu>>>();
+}
