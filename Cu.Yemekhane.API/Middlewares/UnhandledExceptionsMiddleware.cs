566335c8d5802200e6029ab7c21600c874d2fa03
halilkocaoz <halil.i.kocaoz@gmail.com>
2022-10-09 03:21:49 +0300

    webapi: add unhandledexs middleware

---
+++ Cu.Yemekhane.API/Middlewares/UnhandledExceptionsMiddleware.cs
@@ -0,0 +1,40 @@
+using System.Net;
+using Cu.Yemekhane.Common.Models;
+using Newtonsoft.Json;
+
+namespace Cu.Yemekhane.API.Middlewares;
+
+public class UnhandledExceptionsMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public UnhandledExceptionsMiddleware(RequestDelegate next) => _next = next;
+    
+    public async Task Invoke(HttpContext context)
+    {
+        var response = new ApiResponse<object>();
+
+        try
+        {
+            await _next(context);
+        }
+        catch (ApiException exception)
+        {
+            var message = $"Api exception: {exception.Message}.";
+
+            response.ErrorMessage = message;
+            context.Response.StatusCode = 200;
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonConvert.SerializeObject(response));
+        }
+        catch (Exception exception)
+        {
+            var message = $"Internal server error: {exception.Message}.";
+
+            response.ErrorMessage = message;
+            context.Response.StatusCode = 500;
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonConvert.SerializeObject(response));
+        }
+    }
+}
