d9b22926b068eb9ddc18d9629a3e5aa698d99df1
halilkocaoz <halil.i.kocaoz@gmail.com>
2022-10-09 03:18:59 +0300

    webapi: remove menucontroller & modify api to minimal

--- Cu.Yemekhane.API/Program.cs
+++ Cu.Yemekhane.API/Program.cs
@@ -1,34 +1,30 @@
 using Cu.Yemekhane.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddMemoryCache();
 builder.Services.AddSingleton<IWebScrapper, WebScrapper>();
 builder.Services.AddSingleton<IMenuService, MenuService>();
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("default", corsPolicyBuilder =>
     {
-        corsPolicyBuilder
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowAnyOrigin();
+        corsPolicyBuilder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
     });
 });
+
 var app = builder.Build();
-app.MapControllers();
+app.MapGet("menu", (IMenuService menuService) => menuService.GetMenu());
+app.MapGet("menu/{date}", (string date, IMenuService menuService) => menuService.GetMenu(date));
 
 app.UseSwagger();
-app.UseSwaggerUI(conf =>
+app.UseSwaggerUI(c =>
 {
-    conf.SwaggerEndpoint("/swagger/v1/swagger.json", "Cu.Yemekhane.API");
-    conf.RoutePrefix = string.Empty;
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cu.Yemekhane.API");
+    c.RoutePrefix = string.Empty;
 });
+
 app.UseCors("default");
 
 app.UseHttpsRedirection();
